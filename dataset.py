8a7a7770a8e12b6e11a581516e0607eef94ca151
Dmitry <dmitry.korchagin.s@gmail.com>
2024-04-20 12:09:51 +0300

    init

--- dataset.py
+++ dataset.py
@@ -1,42 +1,39 @@
-import numpy as np
-from numpy.typing import NDArray
 from PIL import Image
-from torch import Tensor
-from torchvision import datasets
+from torch.utils.data import Dataset
+import numpy as np
+import os
 
-LABELS_MAP = {
-    0: "background",
-    1: "lunk"
-}
+class LunksDataset(Dataset):
+    def __init__(self, root_dir, transforms=None):
+        self.root_dir = root_dir
+        self.transforms = transforms
+        self.image_paths = []
+        self.mask_paths = []
 
+        # Получаем список файлов в папке Links
+        link_dir = os.path.join(root_dir, 'Lunks')
+        mask_dir = os.path.join(root_dir, 'Masks')
 
-VOC_COLORMAP = [
-    [255, 255, 255],
-    [0, 0, 0]
-]
+        # Перебираем файлы и добавляем изображения и соответствующие маски в списки
+        for file in os.listdir(link_dir):
+            if file.endswith(".png"):  # Предполагаем, что изображения имеют расширения .png
+                image_path = os.path.join(link_dir, file)
+                mask_file = "mask_" + file
+                mask_path = os.path.join(mask_dir, mask_file)
+                if os.path.exists(mask_path):
+                    self.image_paths.append(image_path)
+                    self.mask_paths.append(mask_path)
 
+    def __len__(self):
+        return len(self.image_paths)
 
-class CustomVOCSegmentation(datasets.VOCSegmentation):
-    def __getitem__(self, index: int) -> tuple[Tensor, Tensor]:
-        img = Image.open(self.images[index]).convert("RGB")
-        target = Image.open(self.masks[index]).convert("RGB")
+    def __getitem__(self, index):
+        image = Image.open(self.image_paths[index])
+        mask = Image.open(self.mask_paths[index])
 
-        img = np.array(img, dtype=np.uint8)
-        target = np.array(target, dtype=np.uint8)
-        target = self._convert_to_segmentation_mask(target)
+        if self.transforms:
+            augmented = self.transforms(image=np.array(image), mask=np.array(mask))
+            image = augmented['image']
+            mask = augmented['mask']
 
-        assert self.transform is not None
-
-        augmented = self.transform(image=img, mask=target)
-        return augmented["image"].float(), augmented["mask"].float().permute(2, 0, 1)
-
-    @staticmethod
-    def _convert_to_segmentation_mask(mask: NDArray[np.uint8]) -> NDArray[np.uint8]:
-        height, width = mask.shape[:2]
-        segmentation_mask = np.zeros((height, width, len(VOC_COLORMAP)), dtype=np.uint8)
-        for label_index, label in enumerate(VOC_COLORMAP):
-            segmentation_mask[:, :, label_index] = np.all(
-                mask == label, axis=-1
-            ).astype(np.uint8)
-
-        return segmentation_mask
+        return image, mask
